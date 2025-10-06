3edd0cfc6926b24f6a5eb7c12a0b0f728fafbcd0
rameshAnnabhimoju <ramesh.annabhimoju@gmail.com>
2025-10-06 23:01:25 +0530

    added

--- src/utils/AppUtils.ts
+++
@@ -1,6 +0,0 @@
-export const loadPageData = (location, getRoute) => {
-  const path = location.pathname.replace(/\/$/, "");
-  const data: productDetailsTypes["data"] =
-    (location.state as any)?.data ?? getRoute(path)?.data ?? null;
-  return data;
-};
